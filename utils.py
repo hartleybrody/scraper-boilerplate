ede1549a15659ac9da833fa797e0285e3574d33f
Hartley Brody <hartley.brody@gmail.com>
2019-01-01 12:40:34 -0500

    update utils w proxy, timeouts and logging tweaks

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import os
 import csv
+import random
 import logging
 
 import requests
 from bs4 import BeautifulSoup
-from requests.exceptions import RequestException
+from requests.exceptions import RequestException, Timeout
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
@@ -23,9 +24,12 @@ def make_request(url, headers=None, proxy_check=None, return_type="page"):
         no_proxies("No proxies available for this request")
 
     try:
-        r = requests.get(url, headers=headers, proxies=proxy)
+        r = requests.get(url, headers=headers, proxies=proxy, timeout=10)
     except RequestException as e:
-        logging.warning("Request for {} failed, trying again.".format(url))
+        logging.warning(u"Request failed, trying again: {}".format(e))
+        return make_request(url, headers=headers, proxy_check=proxy_check, return_type=return_type)  # try request again, recursively
+    except Timeout as e:
+        logging.warning(u"Request timed out, trying again: {}".format(e))
         return make_request(url, headers=headers, proxy_check=proxy_check, return_type=return_type)  # try request again, recursively
 
     # proxy_check is a function argument that takes the `requests`
@@ -37,7 +41,7 @@ def make_request(url, headers=None, proxy_check=None, return_type="page"):
             # remove the proxy we used for this request from the
             # PROXIES list so we don't try to reuse it (for now)
             PROXIES.remove(proxy["dict"])
-            logging.warning("Request using {} failed: {}".format(proxy["dict"]["IP"], url))
+            logging.warning("Request using proxy {} failed".format(proxy["dict"]["ip"]))
             return make_request(url, headers=headers, proxy_check=proxy_check, return_type=return_type)  # try request again, recursively
 
     if return_type == "page":
@@ -57,11 +61,9 @@ def get_proxy():
     proxy = PROXIES.pop()  # get the last proxy in the list
     PROXIES.insert(0, proxy)  # put this proxy back at the beginning of the list
 
-    proxy_url = "http://{user}:{pwd}@{ip}:{port}/".format(
-        user=proxy["Login"],
-        pwd=proxy["Password"],
-        ip=proxy["IP"],
-        port=proxy["HTTP port"],
+    proxy_url = "http://{ip}:{port}/".format(
+        ip=proxy["ip"],
+        port=proxy["port"],
     )
 
     # format for request library
@@ -74,14 +76,19 @@ def get_proxy():
 
 def no_proxies(msg):
     if PROXIES_REQUIRED:
-        ScrapingException(msg)
+        raise ScrapingException(msg)
     else:
         logging.warning(msg)
 
 try:
-    with open("proxies.csv", "r") as f:
-        reader = csv.DictReader(f)
-        for line in reader:
-            PROXIES.append(line)
+    with open("input/proxies.txt", "r") as f:
+        for line in f:
+            pieces = line.strip().split(":")
+            PROXIES.append({
+                "ip": pieces[0],
+                "port": pieces[1],
+            })
+    random.shuffle(PROXIES)
+    logging.info("Found {} proxies to use for this scrape".format(len(PROXIES)))
 except:
     no_proxies("No proxies available for this scrape")
